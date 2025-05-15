4ff71c649e1b7fe4a379e55a2168acddecada2cc
Praveen <praveenkasu24@gmail.com>
2025-05-15 18:25:09 -0500

    Updated key pair name and AMI ID in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "ami_id" {
-  default = "ami-00399ec92321828f5" # Amazon Linux 2 AMI for us-east-1
+  default = "ami-0c101f26f147fa7fd" # Amazon Linux 2 AMI for us-east-1
 }
 
 variable "instance_type" {
@@ -12,5 +12,5 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Name of your AWS EC2 key pair"
-  default     = "my-terraform-key" # 🔁 Replace this with your key pair name from AWS
+  default     = "praveen-key" # 🔁 Replace this with your key pair name from AWS
 }
